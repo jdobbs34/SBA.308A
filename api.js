48876ab50407719e93e028b0559b06887b2faa74
jdobbs34 <dobbsjason34@yahoo.com>
2026-01-19 19:45:53 -0700

    9th commit

--- api.js
+++ api.js
@@ -20,7 +20,6 @@ export async function searchRecipesByName(query) {
 
     const data = await response.json();
 
-    
     return data.meals || [];
   } catch (err) {
     console.error("Error searching recipes;", error);
@@ -38,7 +37,6 @@ export async function getRecipesByCategory(category) {
 
     const data = await response.json();
 
-   
     return data.meals || [];
   } catch (err) {
     console.error("Error fetching recipes by category;", error);
@@ -56,7 +54,6 @@ export async function getRecipesByID(id) {
 
     const data = await response.json();
 
-  
     return data.meals ? data.meals[0] : null;
   } catch (err) {
     console.error("Error fetching recipes details;", error);
@@ -80,34 +77,35 @@ export async function getRandomRecipe() {
   }
 }
 
-export async function getRandomRecipe() {
-  try {
-    const response = await fetch(`${API_BASE_URL}/random.php?`);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+export async function getMultipleRandomRecipes(count = 6) {
+  try {
+    const promises = [];
+    for (let i = 0; i < count; i++) {
+      promises.push(getRandomRecipe());
     }
 
-    const data = await response.json();
-    resolve(data.meals[0]);
+
+    const recipes = await Promise.all(promises);
+    return recipes;
   } catch (err) {
-    console.error("Error fetching recipes details;", error);
-    throw new Error("Failed to fetch recipe details.");
+    console.error("Error fetching multip;e random recipes:", error);
+    throw new Error("Failed to fetch recipes.");
   }
 }
 
-export async function getRandomRecipe() {
+export async function getAllCategories() {
   try {
-    const response = await fetch(`${API_BASE_URL}/random.php?`);
+    const response = await fetch(`${API_BASE_URL}/categories.php?`);
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     const data = await response.json();
-    resolve(data.meals[0]);
+    return data.categories || [];
   } catch (err) {
-    console.error("Error fetching recipes details;", error);
-    throw new Error("Failed to fetch recipe details.");
+    console.error("Error fetching categories;", error);
+    throw new Error("Failed to fetch category.");
   }
 }
